b2c8273f098d59a81c12aafa37d2a5d59e3553a4
Roshan K Gujarathi <roshankumargujarathi@gmail.com>
2024-08-13 20:16:02 +0530

    module federation installed

--- apps/roc/src/app/page.tsx
+++ apps/roc/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome roc 👋
+              Welcome ROC 👋
             </h1>
           </div>
 
--- apps/service-suite/src/app/page.tsx
+++ apps/service-suite/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome service-suite 👋
+              Welcome Service Suite 👋
             </h1>
           </div>
 
